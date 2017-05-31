a6d7cd3e08447a104683ccb95045b45b89a698da
Gary Taylor <gary.taylor@hismessages.com>
2017-05-31 19:58:56 +0100

    Exchanged no machine cloud server for normal nx server

--- src/ubuntu/install/nomachine.sh
+++ src/ubuntu/install/nomachine.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Nomachine Cloud Server"
-wget http://download.nomachine.com/download/5.2/Linux/nomachine-cloud-server-evaluation_5.2.21_2_amd64.deb
-dpkg -i nomachine-cloud-server-evaluation_5.2.21_2_amd64.deb
+echo "Nomachine Server"
+wget http://download.nomachine.com/download/5.2/Linux/nomachine_5.2.21_1_amd64.deb
+dpkg -i nomachine_5.2.21_1_amd64.deb
