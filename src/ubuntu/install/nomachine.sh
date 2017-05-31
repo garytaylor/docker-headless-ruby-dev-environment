cc5912dad345f9a8482b99d8ff1159a053bddf97
Gary Taylor <gary.taylor@hismessages.com>
2017-05-31 19:29:51 +0100

    Added nomachine

---
+++ src/ubuntu/install/nomachine.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Nomachine Cloud Server"
+wget http://download.nomachine.com/download/5.2/Linux/nomachine-cloud-server-evaluation_5.2.21_2_amd64.deb
+dpkg -i nomachine-cloud-server-evaluation_5.2.21_2_amd64.deb
